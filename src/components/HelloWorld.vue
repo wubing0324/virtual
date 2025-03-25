9a06a931618845f5e100b636a0737614204e24e3
wubing <wubing@supconit.com>
2025-03-25 16:39:10 +0800

    feat:完成虚拟滚动组件

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,90 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="scroll-container">
+    <virtual-list
+      ref="virtualList"
+      class="list"
+      :data-key="'id'"
+      :data-sources="virtualData"
+      :data-component="ListItem"
+      :estimate-size="50"
+      @scroll="handleScroll"
+    />
   </div>
 </template>
 
 <script>
+import VirtualList from "vue-virtual-scroll-list";
+import ListItem from "./ListItem.vue";
+
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  components: {
+    VirtualList,
+  },
+  data() {
+    return {
+      ListItem: ListItem,
+      originalData: Array.from({ length: 100 }, (_, i) => ({
+        id: i,
+        text: `Item ${i}`,
+      })),
+      scrollSpeed: 1, // 控制滚动速度
+      animationFrameId: null, // 记录 requestAnimationFrame ID
+      isResetting: false, // 是否正在重置滚动位置
+      lastScrollTop: 0, // 记录上次滚动位置
+    };
+  },
+  computed: {
+    virtualData() {
+      // 复制三份数据，确保滚动时的连续性
+      return [...this.originalData, ...this.originalData];
+    },
+  },
+  mounted() {
+    this.startAutoScroll();
+  },
+  methods: {
+    handleScroll(event) {
+      const list = event.target;
+      const scrollHeight = list.scrollHeight / 2; // 获取一组数据的高度
+      console.log('ooooo')
+      // 检测是否是向下滚动
+      const isScrollingDown = list.scrollTop > this.lastScrollTop;
+      this.lastScrollTop = list.scrollTop;
+      console.log(isScrollingDown, list.scrollTop, scrollHeight)
+      if (isScrollingDown && list.scrollTop >= scrollHeight) {
+        // 当滚动到第二组数据时，立即重置到第一组相同位置
+        list.scrollTop = list.scrollTop % scrollHeight;
+        console.log(list.scrollTop, scrollHeight)
+      }
+    },
+    startAutoScroll() {
+      const list = this.$refs.virtualList.$el;
+      const smoothScroll = () => {
+        if (!list) return;
+        list.scrollTop += this.scrollSpeed;
+        this.animationFrameId = requestAnimationFrame(smoothScroll);
+      };
+      this.animationFrameId = requestAnimationFrame(smoothScroll);
+    },
+    stopAutoScroll() {
+      if (this.animationFrameId) {
+        cancelAnimationFrame(this.animationFrameId);
+      }
+    },
+  },
+  beforeDestroy() {
+    this.stopAutoScroll();
+  },
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.scroll-container {
+  height: 300px;
+  border: 1px solid #ddd;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.list {
+  height: 100%;
+  overflow-y: auto;
 }
 </style>
---
+++ src/components/ListItem.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="item">{{ source.text }}</div>
+  </template>
+  
+  <script>
+    export default {
+      name: 'item-component',
+      props: {
+        index: { // index of current item
+          type: Number
+        },
+        source: { // here is: {uid: 'unique_1', text: 'abc'}
+          type: Object,
+          default () {
+            return {}
+          }
+        }
+      }
+    }
+  </script>
+  <style>
+.item {
+    height: 48px;
+    border: 1px solid #ddd;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+</style>
